3b154455ab2a4e7e359b07619ac9c02cace5a14e
unknown <bouyedaniel5@gmail.com>
2025-07-07 18:37:48 +0200

    Affichage des rendez-vous et les modification

--- app/Http/Controllers/RendezVousController.php
+++ app/Http/Controllers/RendezVousController.php
@@ -10,18 +10,23 @@ class RendezVousController extends Controller
 {
     public function index(Request $request)
     {
+        // Récupère l'ID de la laverie connectée ou ciblée
+        $laverieId = auth()->user()->laverie_id; // Ou $request->laverie_id si tu le passes en paramètre
+
+        $facturesQuery = Facture::with('client')
+            ->whereHas('client', function ($query) use ($laverieId) {
+                $query->where('laverie_id', $laverieId);
+            });
+
         if ($request->filled('date_retrait')) {
-            $factures = Facture::with('client')
-                ->whereDate('date_retrait', $request->date_retrait)
-                ->orderBy('date_retrait', 'asc')
-                ->get();
+            $facturesQuery->whereDate('date_retrait', $request->date_retrait);
         } else {
-            $factures = Facture::with('client')
-                ->whereNotNull('date_retrait')
-                ->orderBy('date_retrait', 'asc') // Ordre croissant
-                ->get();
+            $facturesQuery->whereNotNull('date_retrait');
         }
+
+        $factures = $facturesQuery->orderBy('date_retrait', 'asc')->get();
 
         return view('admin.rendezvous.index', compact('factures'));
     }
+
 }
